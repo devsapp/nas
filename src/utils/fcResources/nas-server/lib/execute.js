6fa0e3a725260611cfc6b650b990f8d30fdefcd2
wss-git <wssgryx@163.com>
2021-04-09 10:21:40 +0800

    feat: package nas zip

---
+++ src/utils/fcResources/nas-server/lib/execute.js
@@ -0,0 +1,27 @@
+const { exec } = require('child_process')
+
+function execute(command) {
+  return new Promise((resolve, reject) => {
+    exec(
+      command,
+      {
+        encoding: 'utf8',
+        timeout: 0,
+        maxBuffer: 1024 * 1024 * 1024,
+        killSignal: 'SIGTERM'
+      },
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(error)
+          return
+        }
+        resolve({
+          stdout,
+          stderr
+        })
+      }
+    )
+  })
+}
+
+module.exports = execute
---
+++ src/utils/fcResources/nas-server/lib/path.js
@@ -0,0 +1,19 @@
+const fs = require('fs-extra')
+
+async function makeTmpDir(tmpDir) {
+  let stats
+  let error
+  try {
+    stats = await fs.lstat(tmpDir)
+  } catch (err) {
+    error = err
+  }
+  if (error || (!error && stats.isFile())) {
+    await fs.mkdirp(tmpDir)
+    await fs.chmod(tmpDir, 0o1777)
+  }
+}
+
+module.exports = {
+  makeTmpDir
+}
