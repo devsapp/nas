75cb6627a26dda6642a50c58fbb2ad8eacee361f
wss-git <wssgryx@163.com>
2021-06-22 11:24:17 +0800

    refactor: version 0.0.23

--- dist/utils/utils.d.ts
+++ dist/utils/utils.d.ts
@@ -3,3 +3,4 @@ export declare const sleep: (ms: number) => Promise<unknown>;
 export declare function promptForConfirmContinue(message: string): Promise<boolean>;
 export declare const nasUriHandler: (nasDirYmlInput: string) => string;
 export declare function getMountDir(mountPointDomain: string, nasDir: string): string;
+export declare function transformNasDirPath(url: string): string;
